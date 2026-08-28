1f3d7e7251f319d18d98340933db012343ebabb7
Geoffroy Oscar Davi <oscardev205@gmail.com>
2026-08-28 16:30:01 +0100

    Correction import verifierToken manquant dans auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
+const verifierToken = require('../middleware/auth');
 
 const router = express.Router();
 
